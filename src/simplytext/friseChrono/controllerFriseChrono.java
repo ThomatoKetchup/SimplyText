02391cfb730c86963f87b9332dafcbcad19376c1
ThomatoKetchup <33425389+ThomatoKetchup@users.noreply.github.com>
2017-12-08 11:10:40 +0100

    Création scène frise chrono

---
+++ src/simplytext/friseChrono/controllerFriseChrono.java
@@ -0,0 +1,22 @@
+// LoginControl.java
+package simplytext.friseChrono;
+
+import java.io.IOException;
+import java.net.URL;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
+import javafx.scene.image.ImageView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class controllerFriseChrono {
+
+    @FXML
+    ImageView image;
+    SplitPane splitpane;
+
+    
+}
