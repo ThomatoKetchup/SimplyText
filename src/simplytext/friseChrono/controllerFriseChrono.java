7a6fc1ce62879d70b97ef124d6940e353f9e7fe4
ThomatoKetchup <33425389+ThomatoKetchup@users.noreply.github.com>
2017-12-08 23:46:52 +0100

    Modif frise chro

--- src/simplytext/friseChrono/controllerFriseChrono.java
+++ src/simplytext/friseChrono/controllerFriseChrono.java
@@ -11,12 +11,31 @@ import javafx.scene.image.ImageView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 
 public class controllerFriseChrono {
 
     @FXML
     ImageView image;
     SplitPane splitpane;
+    
+    @FXML
+    Tooltip tooltip1;
+    
 
     
+    @FXML
+	private void process(ActionEvent event) {  //lorsqu'on appuie sur le bouton Action Event event prend le chiffre
+		tooltip1.setText("saluuuut");
+	}
+    
+        
+
+    
+    
+    
+   
 }
+
