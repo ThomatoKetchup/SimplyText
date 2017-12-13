b4052ab40dabd55c7f22b4d7940c38e39396e646
aasofia <33748461+aasofia@users.noreply.github.com>
2017-12-13 01:33:13 +0100

    Page: entrer le texte

--- src/simplytext/MainSimplyText.java
+++ src/simplytext/MainSimplyText.java
@@ -1,18 +1,21 @@
 package simplytext;
 
-import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import simplytext.friseChrono.controllerFriseChrono;
 
 
 public class MainSimplyText extends Application {
     
     @Override
-    public void start(Stage primaryStage) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("ViewSimplyText.fxml"));
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ViewSimplyText.fxml"));
+        Parent root = (Parent)loader.load();
+        controllerFriseChrono monController = (controllerFriseChrono)loader.getController();
+        monController.setStage(primaryStage);
         Scene scene = new Scene(root);
         scene.getStylesheets().addAll(this.getClass().getResource("/simplytext/style/style.css").toExternalForm());
         primaryStage.setScene(scene);
--- src/simplytext/friseChrono/controllerFriseChrono.java
+++ src/simplytext/friseChrono/controllerFriseChrono.java
@@ -12,10 +12,28 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
+import javafx.stage.Stage;
 
 public class controllerFriseChrono {
+    
+    private Stage primaryStage;
+    
+    public void setStage(Stage s) {
+	this.primaryStage = s;
+    }
+    
+    public void processEnter(ActionEvent action) throws Exception {
+	Parent root = FXMLLoader.load(getClass().getResource("viewFriseChrono.fxml"));
+	Scene scene = new Scene(root);
+	this.primaryStage.setScene(scene);
+	this.primaryStage.show();
+    }
+    
+    
 
     @FXML
     ImageView image;
@@ -30,7 +48,8 @@ public class controllerFriseChrono {
 	private void process(ActionEvent event) {  //lorsqu'on appuie sur le bouton Action Event event prend le chiffre
 		tooltip1.setText("saluuuut");
 	}
-    
+        
+          
         
 
     
